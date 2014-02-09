c55b8d10287edbc232f9ea12028de3db164b3340
Aseem Kishore <aseem.kishore@gmail.com>
2014-02-09 16:53:00 -0500

    Implement basic app!

---
+++ app.js
@@ -0,0 +1,28 @@
+
+/**
+ * Module dependencies.
+ */
+
+var express = require('express');
+var http = require('http');
+
+var app = express();
+
+// all environments
+app.set('port', process.env.PORT || 3000);
+app.use(express.favicon());
+app.use(express.logger('dev'));
+app.use(app.router);
+
+// development only
+if ('development' == app.get('env')) {
+  app.use(express.errorHandler());
+}
+
+app.get('*', function (req, res) {
+  res.json(req.headers);
+});
+
+http.createServer(app).listen(app.get('port'), function(){
+  console.log('Express server listening on port ' + app.get('port'));
+});
