2df5ecd9747585acd70fc14fd7f75b36292951ca
Pallav Kothari <pallav.kothari@pallavkoth-ltm.internal.salesforce.com>
2017-07-08 10:41:51 -0700

    derp

--- start-proxy.sh
+++ start-proxy.sh
@@ -6,10 +6,10 @@ echo "starting oauth2_proxy..."
 /app/go/bin/oauth2_proxy  \
 --http-address=http://:$PORT \
 --cookie-secure=false \
---cookie-domain=protected-reef-24329.herokuapp.com \
+--cookie-domain=peaceful-river-83867.herokuapp.com \
 --provider="github" \
 --email-domain="*" \
---redirect-url=https://protected-reef-24329.herokuapp.com/oauth2/callback \
+--redirect-url=https://peaceful-river-83867.herokuapp.com/oauth2/callback \
 --set-xauthrequest=true \
 --upstream=http://127.0.0.1:8080 & 
 
