e78e5aceab2cf13a40dcb0685ad2ff26d30e5c94
Pallav Kothari <pallav.kothari@pallavkoth-ltm.internal.salesforce.com>
2017-07-08 17:07:43 -0700

    tweak start-proxy vars

--- start-proxy.sh
+++ start-proxy.sh
@@ -6,10 +6,10 @@ echo "starting oauth2_proxy..."
 oauth2_proxy  \
 --http-address=http://:$PORT \
 --cookie-secure=false \
---cookie-domain=${APP} \
+--cookie-domain=${COOKIE_DOMAIN} \
 --provider="${PROVIDER}" \
 --email-domain="*" \
---redirect-url=https://${APP}/oauth2/callback \
+--redirect-url=${APP}/oauth2/callback \
 --set-xauthrequest=true \
 --upstream=http://127.0.0.1:8080 & 
 
